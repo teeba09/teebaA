c01eaccc6951788a9774fb97e1ee9898de1b5036
teeba09 <teeba.ali123@yahoo.com>
2020-10-22 02:03:21 +0300

    first commit

---
+++ route/app/v1.ts
@@ -0,0 +1,23 @@
+import * as express from "express";
+const router = express.Router();
+import userAuth from "../../middleware/app/userAuth";
+
+import UserController from "../../controllers/app/user.controller";
+import HomeController from "../../controllers/app/home.controller";
+
+// USER CONTROLLER
+router.post("/register", UserController.register);
+router.post("/otp", UserController.checkOTP);
+router.post("/login", UserController.login);
+router.post("/invoice", userAuth, UserController.makeInvoice);
+router.post("/edit", userAuth, UserController.editInfo);
+router.post("/phone", UserController.phone);
+router.post("/reset", UserController.resetPass);
+
+// HOME CONTROLLERproducts
+router.get("/categories", HomeController.getCategories);
+router.get("/products/:category", HomeController.getProducts);
+router.get("/methods", HomeController.getMethods);
+router.get("/invoices", userAuth, HomeController.getInvoices);
+
+export default router;
